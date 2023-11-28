90e3d0e5ef41cf641457341b25bd81d9d5d467d1
xiongxubang <xubang.xiong.cs@gmail.com>
2023-11-28 21:12:07 +0800

    add scriptGeneraror.py

--- scriptGenerator.py
+++ scriptGenerator.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import os
 
-def makefile_generator(params_path):
-    with open("makefile", "w") as f:
-        f.write("run:\n")
+def script_generator(params_path, file_name = "script.sh"):  
+    with open(file_name, "w") as f:
         table = pd.read_csv(params_path, index_col=0)   # read the hyperParam.csv file using pandas
 
         # Note that there is something wrong with command 'cd'. Please refer to the link: https://blog.csdn.net/robertaqi/article/details/6578449
         for row_num in table.index:
-            f.write(f"\tcd {row_num} && python ./run.py;\n")
+            f.write(f"cd ./{row_num}\n")
+            f.write("python ./run.py;\n")
+            f.write("cd ..\n")
+        
+    os.system(f"chmod +x {file_name}")
 
     
 
@@ -20,7 +23,8 @@ if __name__ == "__main__":
     with open("./bpp_config/files_path.txt", "r") as f:
         lines = f.read().splitlines()   # read all lines
         params_path = lines[0]          # input table   
+        file_name = lines[4]
         
-    makefile_generator(params_path)
+    script_generator(params_path, file_name)
 
 
